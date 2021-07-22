e80af92eab968c9102da9e5a3884b8bab8586d3b
Bhavuk <bhavuksharma2202@gmail.com>
2021-07-23 01:08:34 +0530

    worked on face_data function to collect samples

--- dataset.py
+++ dataset.py
@@ -1,7 +1,10 @@
 import os
+import time
 import cv2 as cv
 
 site = os.path.expanduser("~/.local/lib/python3.9/site-packages/")
+if not (os.path.isdir("./samples")):
+    os.path.mkdir("samples")
 
 
 classifier = cv.CascadeClassifier(
@@ -11,9 +14,27 @@ classifier = cv.CascadeClassifier(
 
 def face_data(classifier):
     capture = cv.VideoCapture(0)
-    _, frame = capture.read()
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    samples, name = 0, "Subject"
+    while True:
+        _, frame = capture.read()
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        values = classifier.detectMultiScale(gray, 1.5, 2)
+        if values is ():
+            print("Camera is unable to detect any face :( pls show yourself")
+            time.sleep(5)
+            continue
+
+        x, y, w, h = values[0]
+        face = gray[y : y + h, x : x + w]
+        samples += 1
+
+        if cv.waitKey(17) == 13 or samples >= 100:
+            break
 
 
-def write_data(face):
-    pass
+def write_data(face, sample, name):
+    fileName = "_".join(name, sample)
+
+
+face_data(classifier)
