d33833d1ae625ad2fd0be9e94382b8fe55ff82ba
Bhavuk <bhavuksharma2202@gmail.com>
2021-07-23 02:04:49 +0530

    updated readme and some minor changes in dataset.py

--- dataset.py
+++ dataset.py
@@ -3,28 +3,31 @@ import time
 import cv2 as cv
 
 site = os.path.expanduser("~/.local/lib/python3.9/site-packages/")
+classifier = cv.CascadeClassifier(
+    f"{site}/cv2/data/haarcascade_frontalface_default.xml"
+)
 
 
 if not (os.path.isdir("./samples")):
     os.mkdir("./samples")
 
 
-classifier = cv.CascadeClassifier(
-    f"{site}/cv2/data/haarcascade_frontalface_default.xml"
-)
-
-
 def face_data(classifier):
     capture = cv.VideoCapture(0)
     samples, name = 0, "Subject"
+    if len(os.listdir("./samples")) >= 90:
+        print(
+            """enough samples are already collected. To collect new samples remove old samples
+        or increase the value of if check in face_data function"""
+        )
     while True:
         _, frame = capture.read()
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         values = classifier.detectMultiScale(gray, 2, 2)
         if values == ():
-            print("Camera is unable to detect any face :( pls show yourself")
-            time.sleep(5)
+            print("""Camera is unable to detect any face :( pls show yourself""")
+            time.sleep(2)
             continue
 
         x, y, w, h = values[0]
@@ -34,17 +37,11 @@ def face_data(classifier):
 
         write_data(face, samples, name)
 
-        if cv.waitKey(17) == 5 or samples >= 100:
+        if cv.waitKey(4) == ord("q") or samples >= 100:
             break
 
 
 def write_data(face, sample: int, name: str):
-    if len(os.listdir("./samples")) > 80:
-        print(
-            """Samples are already recorded. To record new samples delete old samples
-        or increase the number of samples in face_data function"""
-        )
-        return None
     fileName = "./samples/" + "_".join([name, str(sample), ".jpg"])
     cv.imwrite(fileName, face)
 
