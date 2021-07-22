fd77ed2e3dda54828ee0c745ed87607fe4c801fd
Bhavuk <bhavuksharma2202@gmail.com>
2021-07-23 00:22:45 +0530

    function face_data to read face data using cascade classifier

--- dataset.py
+++ dataset.py
@@ -4,7 +4,16 @@ import cv2 as cv
 site = os.path.expanduser("~/.local/lib/python3.9/site-packages/")
 
 
-def dataset():
-    classifier = cv.CascadeClassifier(
-        f"{site}/cv2/data/haarcascade_frontalface_default.xml"
-    )
+classifier = cv.CascadeClassifier(
+    f"{site}/cv2/data/haarcascade_frontalface_default.xml"
+)
+
+
+def face_data(classifier):
+    capture = cv.VideoCapture(0)
+    _, frame = capture.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+
+def write_data(face):
+    pass
