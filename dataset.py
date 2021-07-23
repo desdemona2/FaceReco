0f18ec30d507887f376d69d007c573a150588662
Bhavuk <bhavuksharma2202@gmail.com>
2021-07-23 16:13:56 +0530

    merged with feature

--- dataset.py
+++ dataset.py
@@ -1,5 +1,4 @@
 import os
-import time
 import cv2 as cv
 
 site = os.path.expanduser("~/.local/lib/python3.9/site-packages/")
@@ -20,19 +19,18 @@ def face_data(classifier):
             """enough samples are already collected. To collect new samples remove old samples
         or increase the value of if check in face_data function"""
         )
+        return None
     while True:
         _, frame = capture.read()
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         values = classifier.detectMultiScale(gray, 2, 2)
-        if values == ():
-            print("""Camera is unable to detect any face :( pls show yourself""")
-            time.sleep(2)
+        if len(values) == 0:
             continue
 
         x, y, w, h = values[0]
         face = gray[y : y + h, x : x + w]
-        cv.imshow("View", face)
+        cv.imshow("Face", face)
         samples += 1
 
         write_data(face, samples, name)
